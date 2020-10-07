a2ab726cdcc4231e5257a5464e705edabc107059
Adrian Pop <adrian.pop@liu.se>
2020-10-07 20:16:53 +0200

    a function we can run to test the pipeline

--- src/OM.jl
+++ src/OM.jl
@@ -7,6 +7,14 @@ import DAE
 import HybridDAEParser
 import OMBackend
 
-greet() = print("Fix me!")
+function runModel(modelName::String, modelFile::String)
+  p = HybridDAEParser.parseFile(modelFile)
+  scodeProgram = HybridDAEParser.translateToSCode(p)
+  (dae, cache) = HybridDAEParser.instantiateSCodeToDAE("HelloWorld", scodeProgram)
+  @show dae
+  OMBackend.translate(dae)
+end
+
+# runModel("HelloWorld", "test/HelloWorld.mo")
 
 end # module
