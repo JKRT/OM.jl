f36bf52b2ac7193599df7c2f7ca6a34784ca2877
John <JKRT@users.noreply.github.com>
2020-11-15 18:06:23 +0100

    New test (#7)
    * Added ImmutableList as a submodule
    
    * Divded compilation stages

--- run.jl
+++ run.jl
@@ -1,4 +1,16 @@
-@time using Revise
-@time using OM
+using Revise
+using OM
+@info "Running tests:"
+@time OM.flatten("HelloWorld", "test/HelloWorld.mo")
+@time OM.flatten("VanDerPol", "test/VanDerPol.mo")
+@time OM.flatten("LotkaVolterra", "test/LotkaVolterra.mo")
+@time OM.flatten("BouncingBall", "test/BouncingBall.mo");
 
-@time OM.runModel("LotkaVolterra", "test/LotkaVolterra.mo")
+
+@time OM.runModel("HelloWorld", "test/HelloWorld.mo")
+@time OM.runModel("VanDerPol", "test/VanDerPol.mo")
+@time OM.runModel"LotkaVolterra", "test/LotkaVolterra.mo")
+@time OM.runModel("BouncingBall", "test/BouncingBall.mo");
+
+
+#OM.runModel("SimpleMechanicalSystem", "test/SimpleMechanicalSystem.mo")
--- src/OM.jl
+++ src/OM.jl
@@ -8,15 +8,29 @@ import HybridDAEParser
 import OMBackend
 import Plots
 
-function runModel(modelName::String, modelFile::String; startTime=0.0, stopTime=1.0)
+function flatten(modelName::String, modelFile::String)
   p = HybridDAEParser.parseFile(modelFile)
   scodeProgram = HybridDAEParser.translateToSCode(p)
   (dae, cache) = HybridDAEParser.instantiateSCodeToDAE(modelName, scodeProgram)
-  @show dae
-  OMBackend.translate(dae)
-  Plots.plot(OMBackend.simulateModel(modelName, (startTime, stopTime)))
 end
 
-# runModel("HelloWorld", "test/HelloWorld.mo")
+function runModel(modelName::String, modelFile::String; startTime=0.0, stopTime=1.0)
+  (dae, cache) = flatten(modelName, modelFile)
+  OMBackend.translate(dae)
+  OMBackend.simulateModel(modelName, (startTime, stopTime))
+end
+
+
+function runModelAndPlot(modelName::String, modelFile::String; startTime=0.0, stopTime=1.0)
+  Plots.plot(runModel(modelName, modelFile, startTime = 0.0, stopTime = 1.0))
+end
+
+
+"
+    Turns on debug prints
+"
+function turnOnLogging()
+  ENV["JULIA_DEBUG"] = "OMBackend"
+end
 
 end # module
