04a5d4a74a37f7c098c6e59603435ff04c03e7a1
John Tinnerholm <jtinnerholm@gmail.com>
2020-12-02 14:17:39 +0100

    Updated tests¨

--- run.jl
+++ run.jl
@@ -1,16 +1,22 @@
+#=
+  Simple test file
+=#
 using Revise
 using OM
-@info "Running tests:"
+@info "Running flatten test:"
 @time OM.flatten("HelloWorld", "test/HelloWorld.mo")
 @time OM.flatten("VanDerPol", "test/VanDerPol.mo")
 @time OM.flatten("LotkaVolterra", "test/LotkaVolterra.mo")
 @time OM.flatten("BouncingBall", "test/BouncingBall.mo");
+@time OM.flatten("SimpleMechanicalSystem", "test/SimpleMechanicalSystem.mo")
 
-
+#= Simple Hybrid DAE systems. No hybrid-discrete behaviour=#
+@info "Simulating Simple Hybrid-DAE systems. No hybrid-discrete behavior:"
 @time OM.runModel("HelloWorld", "test/HelloWorld.mo")
 @time OM.runModel("VanDerPol", "test/VanDerPol.mo")
-@time OM.runModel"LotkaVolterra", "test/LotkaVolterra.mo")
-@time OM.runModel("BouncingBall", "test/BouncingBall.mo");
+@time OM.runModel("LotkaVolterra", "test/LotkaVolterra.mo")
+@time OM.runModel("SimpleMechanicalSystem", "test/SimpleMechanicalSystem.mo")
 
+#using Plots How to plot for instance
+#plot(OM.runModel("SimpleMechanicalSystem", "test/SimpleMechanicalSystem.mo"))
 
-#OM.runModel("SimpleMechanicalSystem", "test/SimpleMechanicalSystem.mo")
