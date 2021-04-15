6b67a4c2add8dc9422817115ff91ae1427d97754
John <JKRT@users.noreply.github.com>
2021-04-15 15:14:48 +0200

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,3 @@
-module Test_OM
-
 import Absyn
 import SCode
 import DAE
@@ -7,38 +5,4 @@ import HybridDAEParser
 import OMBackend.jl
 using MetaModelica
 using Test
-
-@testset "MetaModelica" begin
-  @testset "@Module test" begin
-    @test begin
-      try
-        import Absyn
-        import SCode
-        import DAE
-        import HybridDAEParser
-        import OMBackend.jl
-        @test true
-      catch
-        @test false
-      end
-    end
-  end
-
-
-  @testset "@SimplePipeline" begin
-    import Absyn
-    import SCode
-    import DAE
-    import HybridDAEParser
-    import OMBackend.jl
-    p = HybridDAEParser.parseFile("example.mo")
-    scodeProgram = HybridDAEParser.translateToSCode(p)
-    (dae, cache) = HybridDAEParser.instantiateSCodeToDAE("HelloWorld", scodeProgram)
-    OMBackend.translate(dae)
-  end
-
-
-
-end #= End MetaModelica testset =#
-
-end #= End of Test_OM =#
+#= Testar testar=#
