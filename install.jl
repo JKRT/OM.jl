a89042e596adb08fe4aea275847fad634ca50ef3
AnHeuermann <andreas.heuermann@liu.se>
2021-05-07 17:41:17 +0200

    Adding simple install script for OM.jl

---
+++ install.jl
@@ -0,0 +1,30 @@
+using Pkg
+
+@info "Developing sub-packages"
+subPkgs = joinpath.(
+  pwd(),
+  [
+    "ImmutableList.jl",
+    "MetaModelica.jl",
+    "Absyn.jl",
+    "SCode.jl",
+    "DAE.jl",
+    "ArrayUtil.jl",
+    "ListUtil.jl",
+    "OpenModelicaParser.jl",
+    "OMFrontend.jl",
+    "OMBackend.jl",
+  ])
+
+for pkg in subPkgs
+  Pkg.develop(path=pkg)
+end
+
+@info "Developing OM.jl"
+@time Pkg.develop(path=pwd())
+
+@info "precompiling and running tests"
+@time include("run.jl")
+
+@info "Running tests again (but much faster)"
+@time include("run.jl")
