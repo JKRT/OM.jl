3ccf12e845aae700bf2c0c32445d334cc921e626
John Tinnerholm <jtinnerholm@gmail.com>
2021-09-01 18:17:23 +0200

    Updated run file

--- run.jl
+++ run.jl
@@ -20,10 +20,12 @@ end
 """
 function flatten(models, file)
   local scode = OM.translateToSCode("test/$(file).mo")
+  local res
   @info "We have Scode in flatten"
   for model in models
-    local res = OM.OMFrontend.instantiateSCodeToDAE(model, scode)
+    res = OM.OMFrontend.instantiateSCodeToDAE(model, scode)
   end
+  return res
 end
 
 function runModels(models)
@@ -40,32 +42,47 @@ function runModelsMTK(models)
   end
 end
 
-systemsWithoutDifferentials = ["HelloWorldWithoutDer"]
+function runModelsMTK(models, file)
+  for model in models
+    @info "Running : $model"
+    @time OM.runModel(model, "test/$(file).mo", mode = OMBackend.MTK_MODE)
+  end
+end
+
 simpleModelsNoSorting = ["HelloWorld", "LotkaVolterra", "VanDerPol"]
+systemsWithoutDifferentials = ["HelloWorldWithoutDer"]
 simpleModelsSorting = ["SimpleMechanicalSystem"]
 simpleHybridModels = ["BouncingBallReals",
                       "IfEquationDer",
                       "BouncingBallsReal"
                       #=, "ManyEvents5" Currently issues with sundials=#
                       ]
-modelsJustForFlattenTests = ["Influenza", "ElectricalComponentTest"]
 
-@info "Running flatten test:"
-@info "Models that require no sorting"
-flatten(simpleModelsNoSorting)
-@info "Models that require sorting"
-flatten(simpleModelsSorting)
-@info " Hybrid systems"
-flatten(simpleHybridModels)
-@info "Trying to flatten models in a package (Here we have inheritance)"
-compoundModels = ["Models.ManyEvents", "Models.ManyEventsManyConditions"]
-flatten(compoundModels, "ManyEventsPackage")
-@info "Models that we flatten but are still unable to simulate"
-flatten(modelsJustForFlattenTests)
+#modelsJustForFlattenTests = ["Influenza", "ElectricalComponentTest"]
+
+#@info "Models that we flatten but are still unable to simulate"
+#flatten(modelsJustForFlattenTests)
 #= Simple Hybrid DAE systems. No hybrid-discrete behaviour=#
+
+function flattenSimpleModels()
+  @info "Running flatten test:"
+  @info "Models that require no sorting"
+  flatten(simpleModelsNoSorting)
+end
+
+function flattenHybridSystems()
+  flatten(simpleHybridModels)  
+end
+
+function flattenCompoundModels()
+  @info "Trying to flatten models in a package (Here we have inheritance)"
+  compoundModels = ["Models.ManyEvents", "Models.ManyEventsManyConditions"]
+  flatten(compoundModels, "ManyEventsPackage")
+end
+
 function runOriginalBackend()
   runModels(systemsWithoutDifferentials)
-  @info "Simulating Simple Hybrid-DAE systems. No hy  brid-discrete behavior:"
+  @info "Simulating Simple Hybrid-DAE systems. No hybrid-discrete behavior:"
   runModels(simpleModelsNoSorting)
   runModels(simpleModelsSorting)
   @info "Simulating Systems with hybrid behaviour:"
@@ -87,6 +104,28 @@ function runMTKBackend()
   runModelsMTK(simpleModelsSorting)
   @info "Testing hybrid systems with MTK"
   runModelsMTK(simpleHybridModels)
- end
+end
 
-runMTKBackend()
+function flattenAdvancedModels()
+  tst = ["ElectricalComponentTest.ResistorCircuit0"#=, "ElectricalComponentTest.SimpleCircuit"=#]
+  F = "ElectricalComponentTest"
+  tst = ["HelloWorld"#=, "ElectricalComponentTest.SimpleCircuit"=#]
+  F = "HelloWorld"
+  oldRes = flatten(tst, F)
+  for i in 1:100
+    res = flatten(tst, F)
+    if res != oldRes
+      println("Error!")
+    else
+      println("No error")
+      oldRes = res
+    end
+  end
+  
+  #runModelsMTK(tst, F)
+end
+
+flattenSimpleModels()
+flattenAdvancedModels()
+#runMTKBackend()
+#runAdvancedModels()
